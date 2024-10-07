63d237fe30ae253e54d8976eb6b342dad22cc344
NᴏᴄᴏFʟᴜx <abidabdullahown+1@gmail.com>
2024-10-08 00:46:11 +0600

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,8 +1,3 @@
-#(©)CodeXBotz
-
-
-
-
 import os
 import asyncio
 from pyrogram import Client, filters, __version__
@@ -159,7 +154,7 @@ async def not_joined(client: Client, message: Message):
 async def get_users(client: Bot, message: Message):
     msg = await client.send_message(chat_id=message.chat.id, text=WAIT_MSG)
     users = await full_userbase()
-    await msg.edit(f"{len(users)} users are using this bot")
+    await msg.edit(f"{len(users)} users started this bot")
 
 @Bot.on_message(filters.private & filters.command('broadcast') & filters.user(ADMINS))
 async def send_text(client: Bot, message: Message):
