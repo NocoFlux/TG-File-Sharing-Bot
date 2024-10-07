cdf3ccd6620aa8f428fcabbb9f119a3b7e4b4ca3
NᴏᴄᴏFʟᴜx <abidabdullahown+1@gmail.com>
2024-10-08 00:50:57 +0600

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -50,11 +50,11 @@ async def start_command(client: Client, message: Message):
                 ids = [int(int(argument[1]) / abs(client.db_channel.id))]
             except:
                 return
-        temp_msg = await message.reply("Please wait...")
+        temp_msg = await message.reply("Link Generating...")
         try:
             messages = await get_messages(client, ids)
         except:
-            await message.reply_text("Something went wrong..!")
+            await message.reply_text("Something went wrong!I couldn't able to generate link..!")
             return
         await temp_msg.delete()
 
@@ -83,8 +83,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    InlineKeyboardButton("✨About Me", callback_data = "about"),
+                    InlineKeyboardButton("🔓Close", callback_data = "close")
                 ]
             ]
         )
