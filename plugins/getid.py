2b080a45f3d1dbf27c2ba1c47adb22025f93e2a6
NᴏᴄᴏFʟᴜx <abidabdullahown+1@gmail.com>
2024-10-08 00:44:25 +0600

    Create getid.py

---
+++ plugins/getid.py
@@ -0,0 +1,17 @@
+#this code full credit goes to Codeflix Developer 
+
+from pyrogram import filters, enums
+from pyrogram.types import Message
+from bot import Bot
+
+
+@Bot.on_message(filters.command("id") & filters.private)
+async def showid(client, message):
+    chat_type = message.chat.type
+
+    if chat_type == enums.ChatType.PRIVATE:
+        user_id = message.chat.id
+        await message.reply_text(
+            f"<b>ʏᴏᴜʀ ᴜsᴇʀ ɪᴅ ɪs:</b> <code>{user_id}</code>", quote=True
+        )
+      
