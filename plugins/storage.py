8dcd6cec0da266c3fd8004be1cac5adcc2e2b5d7
NᴏᴄᴏFʟᴜx <abidabdullahown+1@gmail.com>
2024-10-08 19:48:09 +0600

    Update storage.py

--- plugins/storage.py
+++ plugins/storage.py
@@ -42,7 +42,7 @@ from config import OWNER_ID  # Import owner ID from config.py
 async def dev_command(client: Client, message: Message):
     await message.reply_text(
         text = f"<b>○   Oᴡɴᴇʀ - </b> <a href='tg://user?id={OWNER_ID}'>Mᴏɴᴋᴇʏ D Lᴜғғʏ</a>\n"
-               f"<b>○   Dᴇᴠᴇʟᴏᴘᴇʀ - </b> <a href='https://t.me/abidabdullah199'>Aɴɪᴍᴇ Qᴜᴇsᴛ</a>\n"
+               f"<b>○   Dᴇᴠᴇʟᴏᴘᴇʀ - </b> <a href='https://t.me/abidabdullah199'>Cʟɪᴄᴋ Hᴇʀᴇ</a>\n"
                f"<b>○   Cʜᴀɴɴᴇʟ - </b> <a href='https://t.me/AnimeQuestX'>Aɴɪᴍᴇ Qᴜᴇsᴛ</a>\n"
                f"<b>○   Dᴏɴɢʜᴜᴀ Cʜᴀɴɴᴇʟ - </b> <a href='https://t.me/DonghuaQuest'>Jᴏɪɴ Nᴏᴡ</a>\n"
                f"<b>○   Oɴɢᴏɪɴɢ Cʜᴀɴɴᴇʟ - </b> <a href='https://t.me/OngoingAnimeQuest'>Jᴏɪɴ Nᴏᴡ</a>\n"
